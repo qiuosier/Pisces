2068aae58e82c5d7b50a54cbbf7cb7e8aa29dfb8
Qiu <qiuosier@gmail.com>
2019-07-29 21:59:44 -0400

    Add reference range to labs.

--- observations.py
+++ observations.py
@@ -5,6 +5,12 @@ class Resources(list):
     def plot(self):
         x = []
         y = []
+        h = []
+        l = []
+        # This function processes only the first reference range.
+        # Multiple reference ranges are interpreted as an "OR".
+        # https://www.hl7.org/fhir/observation-definitions.html#Observation.referenceRange
+        # TODO: Make sure reference range and value have the same unit.
         for resource in self:
             v = resource.get("valueQuantity")
             if not v:
@@ -12,7 +18,24 @@ class Resources(list):
             t = resource.get("effectiveDateTime")
             x.append(t)
             y.append(v.get("value"))
-        fig = PlotlyFigure().line(x, y, name=self[0].get("code").get("text"))
+            r = resource.get("referenceRange")
+            if r:
+                r = r[0]
+                high = r.get("high", {}).get("value")
+                h.append(high)
+                low = r.get("low", {}).get("value")
+                l.append(low)
+            else:
+                h.append(None)
+                l.append(None)
+
+        fig = PlotlyFigure(legend=dict(x=0, y=1.25)).line(
+            x, y, name=self[0].get("code").get("text")
+        ).line(
+            x, h, name="high", line_color='red',
+        ).line(
+            x, l, name="low", line_color='indigo',
+        )
         return fig
 
 
